26e9bfbd3c858ec858eacd3f789f0fe6147f8007
Luke Maxwell <Luke.C.Maxwell-1@ou.edu>
2025-09-10 09:45:43 -0500

    This is the final submission, everything is complete

--- HW1a_Sales/Program.cs
+++ HW1a_Sales/Program.cs
@@ -1,7 +1,7 @@
 ﻿// HW1a Sales Total
 
 // Your Name: Luke Maxwell
-// Did you seek help ? If yes, specify the helper or web link here: 
+// Did you seek help ? If yes, specify the helper or web link here: I used the code that I wrote in class as a guide to help
 
 using System;
 using System.Collections.Generic;
@@ -9,12 +9,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HW1a_Sales
+namespace SaleTotal
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            
+            double num1;
+            double num2;
+
+            double sum1;
+            double sum2;
+            double sum3;
+
+            const double multiplier = 0.085;
+
+            string Num1AsString;
+            string Num2AsString;
+            string Num3AsString;
+
+            Console.WriteLine("What is the item you are purchasing?");
+            Num1AsString = Console.ReadLine();
+            Console.WriteLine("How many " + Num1AsString +"'s are you purchasing?");
+            Num2AsString = Console.ReadLine();
+            Console.WriteLine("What is the price for each " + Num1AsString);
+            Num3AsString = Console.ReadLine();
+
+            num1 = Convert.ToDouble(Num2AsString);
+            num2 = Convert.ToDouble(Num3AsString);
+
+            sum1 = num1 * num2;
+            Console.WriteLine("The subtotal of your bill is $" + sum1.ToString("N2"));
+
+            sum2 = sum1 * multiplier;
+            Console.WriteLine($"The sales tax for your bill is ${sum2}");
+
+            sum3 = sum1 + sum2;
+            Console.WriteLine("The total of your bill is $" + sum3.ToString("N2"));
+            Console.ReadKey();
+
+
         }
     }
 }
--- HW1b_Grade/Program.cs
+++ HW1b_Grade/Program.cs
@@ -1,7 +1,7 @@
 ﻿// HW1b Grade
 
-// Your Name:
-// Did you seek help ? If yes, specify the helper or web link here: 
+// Your Name: Luke Maxwell
+// Did you seek help ? If yes, specify the helper or web link here: I just referred to the code I used on the HW1a assignment
 
 using System;
 using System.Collections.Generic;
@@ -15,6 +15,72 @@ namespace HW1b_Grade
     {
         static void Main(string[] args)
         {
-        }
+            string FirstName;
+            string LastName;
+            int StudentID;
+            string GradeH;
+            string GradeP;
+            string GradeQ;
+            string GradeM; 
+            string GradeF;
+            string FinalGrade;
+
+            double sumH;
+            double sumP;
+            double sumQ;
+            double sumM;
+            double sumF;
+
+            double numH;
+            double numP;
+            double numQ;
+            double numM;
+            double numF;
+
+            const double Hmult = 0.2;
+            const double Pmult = 0.075;
+            const double Qmult = 0.075;
+            const double Mmult = 0.4;
+            const double Fmult = 0.25;
+
+            Console.WriteLine("What is your first name?");
+            FirstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            LastName = Console.ReadLine();
+            Console.WriteLine("What is your student ID?");
+            StudentID = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for homeworks?");
+            GradeH = (Console.ReadLine());
+            Console.WriteLine("What is your overall percentage grade for participations?");
+            GradeP = (Console.ReadLine());
+            Console.WriteLine("What is your overall percentage grade for quizzes?");
+            GradeQ = (Console.ReadLine());
+            Console.WriteLine("What is your overall percentage grade for the midterms?");
+            GradeM = (Console.ReadLine());
+            Console.WriteLine("What is your overall percentage grade for the Final?");
+            GradeF = (Console.ReadLine());
+
+            numH = Convert.ToDouble(GradeH);
+            numP = Convert.ToDouble(GradeP);
+            numQ = Convert.ToDouble(GradeQ);
+            numM = Convert.ToDouble(GradeM);
+            numF = Convert.ToDouble(GradeF);
+
+            sumH = numH * Hmult;
+            sumP = numP * Pmult;
+            sumQ = numQ * Qmult;
+            sumM = numM * Mmult;
+            sumF = numF * Fmult;
+
+            FinalGrade = (sumH + sumP + sumQ + sumM + sumF).ToString("N2");
+            Console.WriteLine(FirstName + " " + LastName + " (" + StudentID + "), your final grade is " + FinalGrade + "%");
+
+
+
+
+
+
+        }   
     }
 }
